e1e50e6bf244d6b47f04be6b6290ace31b9cb5b5
Mor Hadad <mor@simplee.com>
2018-08-09 12:04:58 +0300

    Make a basic user model (including secure password

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,12 +5,18 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Sample App') }
-
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
 
   end
@@ -20,16 +26,15 @@ describe "StaticPages" do
 
     it { should have_selector( 'h1', text: 'Help' ) }
 
-    it {should have_selector('title',text: full_title('Help')) }
+
 
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1', text: 'About') }
 
-    it { should have_selector('title', text: full_title('About Us')) }
+    it { should have_selector('h1', text: 'About') }
 
   end
 
@@ -38,7 +43,21 @@ describe "StaticPages" do
 
     it { should have_selector('h1', text: 'Contact') }
 
-    it { should have_selector('title', text: full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector('title',text: full_title('Help'))
+    click_link "Contact"
+    page.should have_selector('title', text: full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should # fill in
+    click_link "sample app"
+    page.should # fill in
   end
 
 end
